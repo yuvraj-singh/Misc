e5c13adea7f6f3ce380be8b93b60232e9be3963e
yuvraj-singh <yuvrajsingh90014@gmail.com>
2017-06-15 11:27:03 +0530

    added file with weekend data removed and replaced with same day approach

--- Iex_scraping/weekdays.py
+++ Iex_scraping/weekdays.py
@@ -12,22 +12,31 @@ def weekday(d):
     osheet = owb.get_sheet_by_name('Sheet')
     wsheet['A1'] = 'Date'
     wsheet['B1'] = 'MCP'
-    rc=2
+    rc=1
     weekend = 1
-
     for i in range(3,1897):
-        if(weekend>=6):
-            if(weekend==7):
+        if(weekend >= 6):
+            if(weekend == 7):
                 weekend = 1
             else:
-                weekend+=1
+                weekend += 1
         else:
             rc+=1
             wsheet['A'+str(rc)] = osheet['A'+str(i)].value
             wsheet['B'+str(rc)] = osheet['B'+str(i)].value
             weekend+=1
-    print "saving newFileweekday"+str(d)+".xlsx......."
-    wwb.save('newFileweekday'+str(d)+'.xlsx')
+
+    sameday = 89 - (3 * 5)
+    sameday_val = 0
+    for i in range(1,8):
+        if (i   ==  4):
+            sameday += 5
+        else:
+            sameday_val += float(wsheet['B'+str(sameday)].value)
+            sameday += 5
+    wsheet['B'+str(89)] = round(sameday_val / 6, 2)
+    print "saving Fileweekday"+str(d)+".xlsx......."
+    wwb.save('Fileweekday'+str(d)+'.xlsx')
 
 for i in range(1,97):
     weekday(i)
