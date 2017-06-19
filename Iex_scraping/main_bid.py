3d6206271950b6ff3db173956eeaf3bf213a6bf0
yuvraj-singh <yuvrajsingh90014@gmail.com>
2017-06-19 15:14:55 +0530

    added script to download purchase and sell bid along with the MCP

---
+++ Iex_scraping/main_bid.py
@@ -0,0 +1,132 @@
+import os
+import sys
+import datetime
+import openpyxl
+from dateutil.parser import parse
+from subprocess import call
+
+# dt = parse('Feb 15 2010')
+# print(dt)
+# # datetime.datetime(2010, 2, 15, 0, 0)
+# print(dt.strftime('%d/%m/%Y'))
+# # 15/02/2010
+sdate = "05/20/2017" # default starting date
+edate = "05/20/2017" # default  ending date
+
+if len(sys.argv) == 2:
+	sdate = sys.argv[1]
+	edate = sdate
+
+if len(sys.argv) >= 3:
+	sdate = sys.argv[1]
+	edate = sys.argv[2]
+
+format = "EXCELOPENXML"
+if len(sys.argv) >= 4:
+	format = sys.argv[3]
+
+
+def download_files():
+	datediff = 30
+	stemp = parse(sdate)
+	date = stemp + datetime.timedelta(days=datediff-1)
+	fc = 1
+	while (date < parse(edate)):
+		os.system("python download_bid.py"+" "+stemp.strftime('%d/%m/%Y')+" "+date.strftime('%d/%m/%Y')+" "+"BidFile"+str(fc))
+		stemp = date + datetime.timedelta(days=1)
+		date += datetime.timedelta(days=datediff)
+		fc += 1
+	os.system("python download_bid.py"+" "+stemp.strftime('%d/%m/%Y')+" "+parse(edate).strftime('%d/%m/%Y')+" "+"BidFile"+str(fc))
+	return(fc)
+
+def single_file(i):
+	wwb = openpyxl.Workbook()
+	wsheet = wwb.get_sheet_by_name('Sheet')
+	print "opened newbidFile"+str(i)+".xlsx"
+	rc = 2
+	wsheet['A1'] = 'Date'
+	wsheet['B1'] = 'Purchase Bid (MW)'
+	wsheet['C1'] = 'Sell Bid (MW)'
+	wsheet['D1'] = 'MCV (MW)'
+	wsheet['E1'] = 'Cleared Volume (MW)'
+	wsheet['F1'] = 'Final Scheduled Volume (MW)'
+	wsheet['G1'] = 'MCP (Rs/MWh)'
+	for j in range(1,file_count+1):
+		owb = openpyxl.load_workbook("BidFile"+str(j)+".xlsx")
+		osheet = owb.get_sheet_by_name('MarketMinute')
+		# print "file"+str(j)+"opened"
+		k=6+i
+		max_row = 2886
+		if(j<=63):
+			if(j==5):
+				wsheet['A'+str(rc)] = (parse(sdate)+datetime.timedelta(days=rc-2)).strftime('%d/%m/%Y')
+				wsheet['B'+str(rc)] = osheet['D'+str(k)].value
+				wsheet['C'+str(rc)] = osheet['F'+str(k)].value
+				wsheet['D'+str(rc)] = osheet['G'+str(k)].value
+				wsheet['E'+str(rc)] = osheet['H'+str(k)].value
+				wsheet['F'+str(rc)] = osheet['J'+str(k)].value
+				wsheet['G'+str(rc)] = osheet['K'+str(k)].value
+			# print osheet['Q'+str(k)].value
+				rc+=1
+				k+=96
+				wsheet['A'+str(rc)] = (parse(sdate)+datetime.timedelta(days=rc-2)).strftime('%d/%m/%Y')
+				wsheet['B'+str(rc)] = osheet['D'+str(k)].value
+				wsheet['C'+str(rc)] = osheet['F'+str(k)].value
+				wsheet['D'+str(rc)] = osheet['G'+str(k)].value
+				wsheet['E'+str(rc)] = osheet['H'+str(k)].value
+				wsheet['F'+str(rc)] = osheet['J'+str(k)].value
+				wsheet['G'+str(rc)] = osheet['K'+str(k)].value
+			# print osheet['Q'+str(k)].value
+				rc+=1
+				k+=96
+				wsheet['A'+str(rc)] = (parse(sdate)+datetime.timedelta(days=rc-2)).strftime('%d/%m/%Y')
+				wsheet['B'+str(rc)] = 'Yuvraj'
+				wsheet['C'+str(rc)] = 'Yuvraj'
+				wsheet['D'+str(rc)] = 'Yuvraj'
+				wsheet['E'+str(rc)] = 'Yuvraj'
+				wsheet['F'+str(rc)] = 'Yuvraj'
+				wsheet['G'+str(rc)] = 'Yuvraj'
+				rc+=1
+				while(k <= 2790):
+					wsheet['A'+str(rc)] = (parse(sdate)+datetime.timedelta(days=rc-2)).strftime('%d/%m/%Y')
+					wsheet['B'+str(rc)] = osheet['D'+str(k)].value
+					wsheet['C'+str(rc)] = osheet['F'+str(k)].value
+					wsheet['D'+str(rc)] = osheet['G'+str(k)].value
+					wsheet['E'+str(rc)] = osheet['H'+str(k)].value
+					wsheet['F'+str(rc)] = osheet['J'+str(k)].value
+					wsheet['G'+str(rc)] = osheet['K'+str(k)].value
+				# print osheet['Q'+str(k)].value
+					rc+=1
+					k+=96
+			else:
+				while(k <= max_row):
+					wsheet['A'+str(rc)] = (parse(sdate)+datetime.timedelta(days=rc-2)).strftime('%d/%m/%Y')
+					wsheet['B'+str(rc)] = osheet['D'+str(k)].value
+					wsheet['C'+str(rc)] = osheet['F'+str(k)].value
+					wsheet['D'+str(rc)] = osheet['G'+str(k)].value
+					wsheet['E'+str(rc)] = osheet['H'+str(k)].value
+					wsheet['F'+str(rc)] = osheet['J'+str(k)].value
+					wsheet['G'+str(rc)] = osheet['K'+str(k)].value
+				# print osheet['Q'+str(k)].value
+					rc+=1
+					k+=96
+		else:
+			while(k <= 486):
+				wsheet['A'+str(rc)] = (parse(sdate)+datetime.timedelta(days=rc-2)).strftime('%d/%m/%Y')
+				wsheet['B'+str(rc)] = osheet['D'+str(k)].value
+				wsheet['C'+str(rc)] = osheet['F'+str(k)].value
+				wsheet['D'+str(rc)] = osheet['G'+str(k)].value
+				wsheet['E'+str(rc)] = osheet['H'+str(k)].value
+				wsheet['F'+str(rc)] = osheet['J'+str(k)].value
+				wsheet['G'+str(rc)] = osheet['K'+str(k)].value
+			# print osheet['Q'+str(k)].value
+				rc+=1
+				k+=96
+	print "saving newbidFile"+str(i)+".xlsx......."
+	wwb.save('newbidFile'+str(i)+'.xlsx')
+file_count = 64
+# downloading files for data of electricity prices within the date range
+# file_count = download_files()
+# creating new files for each 15 minute block from the downloaded dataMercad
+for i in range(1,97):
+	single_file(i)
---
+++ Iex_scraping/weekday_bid.py
@@ -0,0 +1,49 @@
+import os
+import sys
+import datetime
+import openpyxl
+from dateutil.parser import parse
+from subprocess import call
+
+def weekday(d):
+    wwb = openpyxl.Workbook()
+    wsheet = wwb.get_sheet_by_name('Sheet')
+    owb = openpyxl.load_workbook("newbidFile"+str(d)+".xlsx")
+    osheet = owb.get_sheet_by_name('Sheet')
+    wsheet['A1'] = 'Date'
+    wsheet['B1'] = 'MCP'
+    wsheet['C1'] = 'Purchase Bid'
+    wsheet['D1'] = 'Sell Bid'
+    rc=1
+    weekend = 1
+    for i in range(3,1897):
+        if(weekend >= 6):
+            if(weekend == 7):
+                weekend = 1
+            else:
+                weekend += 1
+        else:
+            rc+=1
+            wsheet['A'+str(rc)] = osheet['A'+str(i)].value
+            wsheet['B'+str(rc)] = osheet['G'+str(i)].value
+            wsheet['C'+str(rc)] = osheet['B'+str(i)].value
+            wsheet['D'+str(rc)] = osheet['C'+str(i)].value
+            weekend+=1
+    sameday_val = [0, 0, 0]
+    column = ['B', 'C', 'D']
+    for col in range(1,4):
+        sameday = 89 - (3 * 5)
+        for i in range(1,8):
+            if (i   ==  4):
+                sameday += 5
+            else:
+                sameday_val[col-1] += float(wsheet[column[col-1]+str(sameday)].value)
+                sameday += 5
+    wsheet['B'+str(89)] = round(sameday_val[0] / 6, 2)
+    wsheet['C'+str(89)] = round(sameday_val[1] / 6, 2)
+    wsheet['D'+str(89)] = round(sameday_val[2] / 6, 2)
+    print "saving BidFileweekday"+str(d)+".xlsx......."
+    wwb.save('BidFileweekday'+str(d)+'.xlsx')
+
+for i in range(1,97):
+    weekday(i)
